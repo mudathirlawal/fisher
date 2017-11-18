796393a36de1172511b20332d73911b31d976800
Surajudeen Akande <surajudeen.akande@andela.com>
2017-11-18 14:38:10 +0100

    Fixed mailing and AJAX response

--- fresh/contactform/contactform.php
+++ fresh/contactform/contactform.php
@@ -1,34 +1,32 @@
 <?php
-/* 
-    PHP contact form script 
+/*
+    PHP contact form script
 */
 
 /***************** Configuration *****************/
 
-  require_once('vendor/autoload.php');
+  require_once('../vendor/autoload.php');
 
-  //use PHPMailer;
-
-  $mail = new PHPMailer;
+  $mail = new \PHPMailer\PHPMailer\PHPMailer();
 
   // Email information
-  $contact_email_to = "deenbey3@gmail.com";
+  $contact_email_to = "surajudeen.akande@andela.com";
   $email = $_POST['email'];
   $name = $_POST['name'];
   $message = $_POST['message'];
   $subject = $_POST['subject'];
 
-  $mail->SMTPDebug = 2;
   $mail->isSMTP();                                 // Set mailer to use SMTP
-  $mail->Host = 'mail.nxygeoprudentials.com';      // Specify main and backup SMTP servers
+//  $mail->SMTPDebug = 2;
+  $mail->Host = 'smtp.gmail.com';      // Specify main and backup SMTP servers
   $mail->SMTPAuth = true;                          // Enable SMTP authentication
-  $mail->Username = 'info@nxygeoprudentials.com';  // SMTP username
-  $mail->Password = '*$KILIMANJAR0';                // SMTP password
+  $mail->Username = '';  // SMTP username
+  $mail->Password = '';                // SMTP password
   $mail->SMTPSecure = 'ssl';                       // Enable TLS encryption, `ssl` also accepted
   $mail->Port = 465;                               // TCP port to connect to
 
 // Subject prefix
-$contact_subject_prefix = "Contact Form Message: ";    
+$contact_subject_prefix = "Contact Form Message: ";
 
 // Name too short error text
 $contact_error_name = "Name is too short or empty!";
@@ -46,8 +44,8 @@ $contact_error_message = "Too short message! Please enter something.";
 
 if(!isset($_SERVER['HTTP_X_REQUESTED_WITH']) AND strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) != 'xmlhttprequest') {
     // The Request must be Ajax POST, enter a message for direct access requests.
-    die('Invalid Request!'); 
-} 
+    die('Invalid Request!');
+}
 
 if( isset($_POST) ) {
 
@@ -55,42 +53,38 @@ if( isset($_POST) ) {
     $email = filter_var($_POST["email"], FILTER_SANITIZE_EMAIL);
     $subject = filter_var($_POST["subject"], FILTER_SANITIZE_STRING);
     $message = filter_var($_POST["message"], FILTER_SANITIZE_STRING);
-    
-    if(strlen($name)<4){ 
+
+    if(strlen($name)<4){
         die($contact_error_name);
     }
-    
-    if(!filter_var($email, FILTER_VALIDATE_EMAIL)){  
+
+    if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
         die($contact_error_email);
     }
-    
-    if(strlen($subject)<3){ 
+
+    if(strlen($subject)<3){
         die($contact_error_subject);
     }
-                    
-    if(strlen($message)<3){ 
+
+    if(strlen($message)<3){
         die($contact_error_message);
     }
 
-  $mail->setFrom("$email", 'Client Mail');    
+  $mail->setFrom("$email", 'Client Mail');
   $mail->addAddress($contact_email_to, 'Admin');
   $mail->isHTML(true);
   $mail->Subject = $subject;
   $mail->Body    = "Sender Email: ". $email ."<br />".
                     $message;
-  $sendemail = send($contact_email_to, $contact_subject_prefix . $subject, $message,
+  $sendEmail = $mail->send($contact_email_to, $contact_subject_prefix . $subject, $message,
          "From: ".$name." <".$email.">" . PHP_EOL
         ."Reply-To: ".$email . PHP_EOL
         ."X-Mailer: PHP/" . phpversion()
     );
-    
-    if( $sendemail ) {
-        echo 'OK';
+
+    if( $sendEmail ) {
+        return json_encode(['status_code' => 200]);
     } else {
-        echo 'Could not send mail! Please check your PHP mail configuration.';
+      return json_encode(['status_code' => $mail->ErrorInfo]);
     }
 }
-//var_dump($email);
-
-
-
